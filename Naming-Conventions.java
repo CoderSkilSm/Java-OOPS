9358825d2ed8f7985079c7c5bb593cccc423d24c
CoderSkilSm <optimal844@gmail.com>
2024-10-18 17:20:31 +0530

    Create Naming-Conventions.java

---
+++ Naming-Conventions.java
@@ -0,0 +1,20 @@
+
+
+//Encapsulation ---> means combining data and methods altogether
+
+public class HelloWorld {
+    public static void main(String[] args){
+      System.out.println("Naming-Conventions")
+    }
+}
+
+//Camel casing
+
+//class and interface -> start with capital letters
+//variable and methods -> start with small letters
+//constants --> all with capital letters(PIE,BRAND)
+//showMyMarks() 
+//age --> variable
+//DATA --> CONSTANT
+//Human() --> Constructor
+
